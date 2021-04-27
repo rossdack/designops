a4982a8c95baa5881018633e74d00a56ff384442
amendon <ashwin.mendon@ausnetservices.com.au>
2021-04-27 10:44:57 +1000

    added signin component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import StickyFooter from './Components/StickyFooter';
 import Album from './Components/Album';
-// import SignIn from './Components/SignIn';
 
 function App() {
   return (
     <div>
-      {/* <SignIn /> */}
       <Album Heading="Photo Album" SubContent="Something short and leading about the collection below—its contents, the creator, etc.
               Make it short and sweet, but not too short so folks don&apos;t simply skip over it
               entirely." />
