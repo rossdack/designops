612f9f9e5604fb377019b9e871bce9298ad3d83a
amonotm11 <mills.adam@yahoo.com.au>
2022-06-10 14:05:11 +1000

    Got our sticky footer working and deleted examples

--- src/stories/StickyFooter.stories.jsx
+++ src/stories/StickyFooter.stories.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
-import { Button } from './Button';
 import StickyFooter from '../lib/StickyFooter';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Real/Test',
+  title: 'DesignOps/StickyFooter',
   component: StickyFooter,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
@@ -16,12 +15,18 @@ export default {
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => <StickyFooter {...args} />;
 
-export const Primary = Template.bind({});
+export const DefaultSettings = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
+DefaultSettings.args = {
+
+};
+
+export const Overridden = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Overridden.args = {
   copyright: 'Copyright Text',
   footerText: 'Footer Text',
-  position: 'center',
+  position: 'left',
   children: ''
 };
 
